da1bc77d4960aaa1514fc3234497de3ceef5596f
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-07-07 14:23:08 -0300

    Material-UI configuratioin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
 export default function Home() {
   return (
-    <div>Kp</div>
+    <Button variant="outlined" color="secondary">
+      Primary
+    </Button>
   );
 }
