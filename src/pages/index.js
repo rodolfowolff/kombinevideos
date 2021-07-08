230c8a4241a0680c9e06fa892d68104965b6fd93
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-07-08 15:24:47 -0300

    get videos from mongoDB

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
 import { Box, Grid } from '@material-ui/core';
 
 import Layout from 'src/components/Layout';
 import VideoCard from 'src/components/VideoCard';
+import { getVideos } from 'src/database/getvideos';
 
 function Home({ data }) {
   return (
@@ -23,52 +23,32 @@ function Home({ data }) {
 
 export async function getStaticProps() {
 
-  const data = [
-    {
-      id: 1,
-      title: 'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
-      authorId: 1,
-      authorName: 'Lucas Nhimi',
-      authorAvatar: 'avatarUrl',
-      views: 10,
-      thumb: '/new-video.svg',
-      videoUrl: 'url',
-      updatedAt: new Date(),
-    },
-    {
-      id: 2,
-      title: 'XXX DO XXXXX | XX XX XX XXXXXXXXXXXXXXXXXX',
-      authorId: 1,
-      authorName: 'XX XXX',
-      authorAvatar: 'avatarUrl',
-      views: 10,
-      thumb: '/new-video.svg',
-      videoUrl: 'url',
-      updatedAt: new Date(),
-    },
-    {
-      id: 3,
-      title: 'XXX DO XXXXX | XX XX XX XXXXXXXXXXXXXXXXXX',
-      authorId: 1,
-      authorName: 'XX XXX',
-      authorAvatar: 'avatarUrl',
-      views: 10,
-      thumb: '/new-video.svg',
-      videoUrl: 'url',
-      updatedAt: new Date(),
-    },
-    {
-      id: 4,
-      title: 'XX XXXXX ',
-      authorId: 1,
-      authorName: 'XX XXX',
-      authorAvatar: 'avatarUrl',
-      views: 10,
-      thumb: '/new-video.svg',
-      videoUrl: 'url',
-      updatedAt: new Date(),
-    },
-  ];
+  // const data = [
+  //   {
+  //     id: 1,
+  //     title: 'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+  //     authorId: 1,
+  //     authorName: 'Lucas Nhimi',
+  //     authorAvatar: 'avatarUrl',
+  //     views: 10,
+  //     thumb: '/new-video.svg',
+  //     videoUrl: 'url',
+  //     updatedAt: new Date(),
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'XXX DO XXXXX | XX XX XX XXXXXXXXXXXXXXXXXX',
+  //     authorId: 1,
+  //     authorName: 'XX XXX',
+  //     authorAvatar: 'avatarUrl',
+  //     views: 10,
+  //     thumb: '/new-video.svg',
+  //     videoUrl: 'url',
+  //     updatedAt: new Date(),
+  //   },
+  // ];
+
+  const data = await getVideos();
 
   return {
     props: { data: JSON.parse(JSON.stringify(data)) },
